f621bbf9a82b29d6f84b6d7c63e449775190dd6e
singhudit8935 <singhudit5891@gmail.com>
2024-07-02 19:49:51 +0530

    main

--- IR_and_GPR.v
+++ IR_and_GPR.v
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 
-//defining field of IR
+// defining field of IR----------------_______--------____------__--------
 `define oper_type IR[31:27]
 `define rdst IR[26:22]
 `define rsrc1 IR[21:17]
@@ -8,58 +8,79 @@
 `define rsrc2 IR[15:11]
 `define isrc IR[15:0] // when working in immediate mode 
 
-// arithmetic operations 
+// arithmetic operations -----------------------------------
 `define movsgpr 5'b00000
 `define mov 5'b00001
 `define add 5'b00010
 `define sub 5'b00011
 `define mul 5'b00100
 
+// logical instructions-----------------------------or, and, xor, xnor, nor, not, nand 
+`define ror 5'b00101  // we are not using OR because it is a keyword. We can't use a keyword for naming a variable
+`define rand 5'b00110
+`define rxor 5'b00111
+`define rxnor 5'b01000
+`define rnand 5'b01001
+`define rnor 5'b01010
+`define rnot 5'b01011
+
 module top();
-    reg[31:0] IR;  // [31-27] bits are used for operation type , [26-22] are used for destination register address
-                   // [21-27] are used for source register address 1, bit 16 is used for mode selection
-                   // if mode is immediate address mode then [15-0] are used for storing operand otherwise [15-11] are used for source address 2
+    reg [31:0] IR;  // [31:27] bits are used for operation type, [26:22] are used for destination register address
+                    // [21:17] are used for source register address 1, bit 16 is used for mode selection
+                    // if mode is immediate address mode then [15:0] are used for storing operand otherwise [15:11] are used for source address 2
     
-    reg[15:0] GPR[31:0];     //adding 16 general purpose registers of 32 bits each
-    reg[15:0] SGPR  ;             //adding a speacial gneral purpose register to store remaining 16 bits of product term
-    reg[31:0] mul_result;      // adding a temporary reg to store 32 bits of multiplication
+    reg [15:0] GPR [31:0]; // 32 general purpose registers of 16 bits each
+    reg [15:0] SGPR;       // special general purpose register to store remaining 16 bits of product term
+    reg [31:0] mul_result; // temporary register to store 32 bits of multiplication
 
     always @(*) begin
         case (`oper_type)
-            //case1 of move special
+            // case 1: move special
             `movsgpr: begin
                 GPR[`rdst] = SGPR;
             end
-            //case2 of move
+            // case 2: move
             `mov: begin
-                if(`imm_mode)
-                    GPR[`rdst]= `isrc;
+                if (`imm_mode)
+                    GPR[`rdst] = `isrc;
                 else
-                    GPR[`rdst]= `rsrc2;
-                end
-            //case 3 of addition
+                    GPR[`rdst] = GPR[`rsrc2];
+            end
+            // case 3: addition
             `add: begin
-                if(`imm_mode)
-                    GPR[`rdst]= GPR[`rsrc1] + `isrc;
+                if (`imm_mode)
+                    GPR[`rdst] = GPR[`rsrc1] + `isrc;
                 else
-                    GPR[`rdst]= GPR[`rsrc1] + GPR[`rsrc2];
+                    GPR[`rdst] = GPR[`rsrc1] + GPR[`rsrc2];
             end
-            //case 4 of subtraction
-            `sub : begin
-                 if(`imm_mode)
-                    GPR[`rdst]= GPR[`rsrc1] - `isrc;
+            // case 4: subtraction
+            `sub: begin
+                if (`imm_mode)
+                    GPR[`rdst] = GPR[`rsrc1] - `isrc;
                 else
-                    GPR[`rdst]= GPR[`rsrc1] - GPR[`rsrc2];
+                    GPR[`rdst] = GPR[`rsrc1] - GPR[`rsrc2];
             end
-            //case 5 of multiplication
+            // case 5: multiplication
             `mul: begin
-                 if(`imm_mode)
-                   mul_result = GPR[`rsrc1] * `isrc;
+                if (`imm_mode)
+                    mul_result = GPR[`rsrc1] * `isrc;
                 else
-                    mul_result= GPR[`rsrc1] * GPR[`rsrc2];
+                    mul_result = GPR[`rsrc1] * GPR[`rsrc2];
+                GPR[`rdst] = mul_result[15:0];
+                SGPR = mul_result[31:16];
+            end
+            // case 6: logical OR
+            `ror: begin
+                if (`imm_mode)
+                    GPR[`rdst] = GPR[`rsrc1] | `isrc;
+                else
+                    GPR[`rdst] = GPR[`rsrc1] | GPR[`rsrc2];
+            end
+
+            default: begin
+                // Default case to handle undefined operations
+                GPR[`rdst] = 16'b0;
             end
         endcase
     end
-  
-
 endmodule
--- rough.v
+++ rough.v
@@ -1,76 +1,86 @@
-`include "IR_and_GPR.v"
-module tb;
-integer i=0;
-top dut();
+`timescale 1ns / 1ps
 
-//updating all GPR to 2
-initial begin
-    for(i=0; i<32; i=i+1) begin
-        dut.GPR[i]=2;
-        
+// defining field of IR----------------_______--------____------__--------
+`define oper_type IR[31:27]
+`define rdst IR[26:22]
+`define rsrc1 IR[21:17]
+`define imm_mode IR[16]
+`define rsrc2 IR[15:11]
+`define isrc IR[15:0] // when working in immediate mode 
+
+// arithmetic operations -----------------------------------
+`define movsgpr 5'b00000
+`define mov 5'b00001
+`define add 5'b00010
+`define sub 5'b00011
+`define mul 5'b00100
+
+// logical instructions-----------------------------or, and, xor, xnor, nor, not, nand 
+`define ror 5'b00101  // we are not using OR because it is a keyword. We can't use a keyword for naming a variable
+`define rand 5'b00110
+`define rxor 5'b00111
+`define rxnor 5'b01000
+`define rnand 5'b01001
+`define rnor 5'b01010
+`define rnot 5'b01011
+
+module top();
+    reg [31:0] IR;  // [31:27] bits are used for operation type, [26:22] are used for destination register address
+                    // [21:17] are used for source register address 1, bit 16 is used for mode selection
+                    // if mode is immediate address mode then [15:0] are used for storing operand otherwise [15:11] are used for source address 2
+    
+    reg [15:0] GPR [31:0]; // 32 general purpose registers of 16 bits each
+    reg [15:0] SGPR;       // special general purpose register to store remaining 16 bits of product term
+    reg [31:0] mul_result; // temporary register to store 32 bits of multiplication
+
+    always @(*) begin
+        case (`oper_type)
+            // case 1: move special
+            `movsgpr: begin
+                GPR[`rdst] = SGPR;
+            end
+            // case 2: move
+            `mov: begin
+                if (`imm_mode)
+                    GPR[`rdst] = `isrc;
+                else
+                    GPR[`rdst] = GPR[`rsrc2];
+            end
+            // case 3: addition
+            `add: begin
+                if (`imm_mode)
+                    GPR[`rdst] = GPR[`rsrc1] + `isrc;
+                else
+                    GPR[`rdst] = GPR[`rsrc1] + GPR[`rsrc2];
+            end
+            // case 4: subtraction
+            `sub: begin
+                if (`imm_mode)
+                    GPR[`rdst] = GPR[`rsrc1] - `isrc;
+                else
+                    GPR[`rdst] = GPR[`rsrc1] - GPR[`rsrc2];
+            end
+            // case 5: multiplication
+            `mul: begin
+                if (`imm_mode)
+                    mul_result = GPR[`rsrc1] * `isrc;
+                else
+                    mul_result = GPR[`rsrc1] * GPR[`rsrc2];
+                GPR[`rdst] = mul_result[15:0];
+                SGPR = mul_result[31:16];
+            end
+            // case 6: logical OR
+            `ror: begin
+                if (`imm_mode)
+                    GPR[`rdst] = GPR[`rsrc1] | `isrc;
+                else
+                    GPR[`rdst] = GPR[`rsrc1] | GPR[`rsrc2];
+            end
+
+            default: begin
+                // Default case to handle undefined operations
+                GPR[`rdst] = 16'b0;
+            end
+        endcase
     end
-end
-
-initial begin
-    //testing immediate operation
-    $display("---------------");
-    dut.IR=0;
-    dut.`imm_mode=1;
-    dut.`oper_type=2;
-    dut.`rsrc1=2; //gpr[2] =2
-    dut.`rdst=0; // gpr[0]
-    dut.`isrc=4;  //
-
-    #10;
-
-    $display("OP:ADI Rsrc1:%0d Rsrc2:%0d  Rdst:%0d", dut.GPR[2], dut.`isrc, dut.GPR[0]);
-    $display("-----------------");
-    //register add operation
-
-    dut.IR=0;
-    dut.`imm_mode=0;
-    dut.`oper_type=2;
-    dut.`rsrc1=4;
-    dut.`rdst=0;
-    dut.`rsrc2=5;
-
-    #10;
-    $display("OP:ADI Rsrc1:%0d Rsrc2:%0d  Rdst:%0d", dut.GPR[4], dut.GPR[5], dut.GPR[0]);
-    $display("-----------------");
-
-    //immediate mov operation
-    dut.IR=0;
-    dut.`imm_mode=1;
-    dut.`oper_type=1;
-    dut.`rdst=4; //gpt[4]
-    dut.`isrc=55;
-    #10;
-    $display("OP:MOVI Rdst:%0d imm_data:%0d", dut.GPR[4], dut.`isrc);
-    $display("--------------");
-
-    //egister mov
-    dut.IR=0;
-    dut.`imm_mode=0;
-    dut.`oper_type=1;
-    dut.`rdst=4; //gpt[4]
-    dut.`rsrc1=7; //gpr[7]
-    #10;
-    $display("OP:MOV Rdst:%0d imm_data:%0d", dut.GPR[4],dut.GPR[7]);
-    $display("--------------");
-
-    
-
-
-
-
-
-
-    
-end
-
-
-
-
-
-
 endmodule
